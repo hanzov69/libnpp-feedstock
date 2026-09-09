cdc5a5c98f1f46d45136f3c579c32a941df8cfeb
Mr. Christian <chrisullivan@nvidia.com>
2026-09-09 16:30:22 -0400

    Update for CUDA 13.4.1 (#35)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,12 @@
-if not exist %PREFIX% mkdir %PREFIX%
-if not exist %LIBRARY_LIB%\x64 mkdir %LIBRARY_LIB%\x64
+if "%TARGET_PLATFORM%" == "win-arm64" (
+    set CUDA_ARCH=arm64
+) else (
+    set CUDA_ARCH=x64
+)
 
-move lib\x64\* %LIBRARY_LIB%\x64
-move bin\x64\* %LIBRARY_BIN%
+if not exist %PREFIX% mkdir %PREFIX%
+if not exist %LIBRARY_LIB%\%CUDA_ARCH% mkdir %LIBRARY_LIB%\%CUDA_ARCH%
+
+move lib\%CUDA_ARCH%\* %LIBRARY_LIB%\%CUDA_ARCH%
+move bin\%CUDA_ARCH%\* %LIBRARY_BIN%
 move include\* %LIBRARY_INC%
